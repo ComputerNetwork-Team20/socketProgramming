db208b1dbeb599d989f7f9c5fd473871d1e71af6
YuJin Kwon <tokyj515@naver.com>
2023-11-07 19:35:30 +0900

    feat: 무한루프 해결 코드

--- Client.py
+++ Client.py
@@ -2,31 +2,45 @@
 import socket
 from _thread import *
 
-HOST = '127.0.0.1'
-PORT = 9999
 
-
-client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM) # 소켓 생성
-client_socket.connect((HOST, PORT))  #연결
 
 # 서버로부터 메세지를 받는 메소드
 # 스레드로 구동 시켜, 메세지를 보내는 코드와 별개로 작동하도록 처리
 def recv_data(client_socket) :
     while True :
-        data = client_socket.recv(1024)
-        print("MENTION:" + repr(data.decode()))
-        if(data=="GAME OVER"):  client_socket.close()
+        try:
+            data = client_socket.recv(1024)
+            print("MENTION:" + data.decode())
+            if(data=="GAME OVER"):
+                break
+        except ConnectionResetError as e:
+            print('>> Disconnected by ' + addr[0], ':', addr[1])
+            break
 
 
 if __name__ == '__main__':
+    print('>>> 클라이언트 실행')
 
-    start_new_thread(recv_data, (client_socket,))
-    print('>> Connect Server')
+    HOST = '127.0.0.1'
+    PORT = 9999
 
-    while True:
-        message = input('>>알파벳 혹은 단어를 입력하세요')
-        client_socket.send(message.encode())
 
+    try:
+        while True:
+            client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # 소켓 생성
+            client_socket.connect((HOST, PORT))  # 연결
+            print('>> Connect Server')
+
+
+            message = input('>>> 알파벳 혹은 단어를 입력하세요')
+            start_new_thread(recv_data, (client_socket,))
+            client_socket.send(message.encode())
+
+
+    except Exception as e:
+        print('에러는? : ', e)
+    finally:
+        client_socket.close()
 
 
 
